35a66e479d509139ff67d1ea5e0bc3df8f3e475d
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-05-22 23:45:32 +0200

    Created custom User model,added migrations and dependencies

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    is_host = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=20, blank=True)
+    avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
 # Create your models here.
