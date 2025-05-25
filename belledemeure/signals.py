36041242511eebdda50e1c54a5642593cc5fdae8
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-05-26 00:22:23 +0200

    Fixed signals for users and properties

--- belledemeure/signals.py
+++ belledemeure/signals.py
@@ -1,9 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.conf import settings
-from .models import User
+from users.models import Profile
+from .utils import send_welcome_email
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+User = get_user_model()
+
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        User.objects.create(user=instance)
+        Profile.objects.create(user=instance)
+        if instance.email:
+            send_welcome_email(instance.email, instance.username)
