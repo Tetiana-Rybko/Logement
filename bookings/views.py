4c4cdf357339ae2cedc0d045104bac85cc0836ac
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-05-26 01:02:14 +0200

    Added two views: for creating/viewing all bookings and for working with a specific one.

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,23 @@
+from rest_framework import generics, permissions
+from bookings.models import Booking
+from bookings.serializers import BookingSerializer
+
+
+class BookingListCreateView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class BookingDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
 from django.shortcuts import render
 
 # Create your views here.
