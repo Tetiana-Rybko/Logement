c13fbf40627150fc57bf1f24e15fa41e3c81cda1
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-05-26 10:01:30 +0200

    Restricted bookings visibility: regular users see only their own

--- bookings/views.py
+++ bookings/views.py
@@ -1,23 +1,31 @@
 from rest_framework import generics, permissions
 from bookings.models import Booking
 from bookings.serializers import BookingSerializer
+from django.shortcuts import render
 
 
 class BookingListCreateView(generics.ListCreateAPIView):
-    queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Booking.objects.all()
+        return Booking.objects.filter(user=user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 
 class BookingDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Booking.objects.all()
+        return Booking.objects.filter(user=user)
 
-from django.shortcuts import render
 
-# Create your views here.
