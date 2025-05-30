019b6b25a0ff39d979cd0855c3430180cc637cbf
Tetiana-Rybko <tanyarybko28@gmail.com>
2025-05-31 00:26:33 +0200

    Registration  models

--- belledemeure/admin.py
+++ belledemeure/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Property, Amenity
 
-# Register your models here.
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'address', 'price_per_night', 'is_available')
+    list_filter = ('is_available', 'has_furniture', 'has_appliances')
+    search_fields = ('title', 'description', 'address')
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
